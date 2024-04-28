509e5fdf9f0190d2c79806bd119d78718897e925
harshbhar0629 <harshbhar.0629@gmail.com>
2024-04-28 12:07:03 +0530

    Adding css to card

--- React/testimonial_slider/src/App.js
+++ React/testimonial_slider/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
 		<div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
 			<div className="text-center">
 				<h1 className="text-4xl font-bold">Our Testimonials</h1>
-				<div></div>
+				<div className="bg-violet-400 h-[4px] w-[180px] mt-1 mx-auto rounded-md"></div>
 			</div>
 			<Testimonial reviews={reviews}></Testimonial>
 		</div>
