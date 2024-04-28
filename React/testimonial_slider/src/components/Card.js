23ad1382e585fe7bc812481455939470211c3b45
harshbhar0629 <harshbhar.0629@gmail.com>
2024-04-28 12:55:24 +0530

    Adding complete css to Testimonial:

--- React/testimonial_slider/src/components/Card.js
+++ React/testimonial_slider/src/components/Card.js
@@ -4,54 +4,36 @@ import {
 	FaQuoteRight,
 } from "react-icons/fa";
 
-import {
-	FiChevronLeft,
-	FiChevronRight,
-} from "react-icons/fi";
 
 function Card(props) {
     let review = props.review;
     return (
-			<div className="flex flex-col sm:relative">
+			<div className="flex flex-col relative">
 				<div className="absolute top-[-7rem] z-[10] mx-auto">
 					<img
 						className=" aspect-sqaure rounded-full w-[110px] h-[110px] z-[12]"
 						src={review.image}></img>
-					<div className="w-[110px] h-[110px] bg-violet-500 rounded-full absolute top-[-6px] z-[-10] left-[10px]"></div>
+					<div className="w-[110px] h-[110px] bg-violet-500 rounded-full absolute top-[-4px] z-[-10] left-[8px]"></div>
 				</div>
 
 				<div className="text-center mt-7">
-					<p className="font-bold text-2xl capitalize">{review.name}</p>
+                <p className="font-bold text-2xl capitalize">{review.name}</p>
 				</div>
 
-				<div className="text-center mt-7">
-					<p className="text-violet-400 uppercase text-sm">{review.job}</p>
+				<div className="text-center">
+					<p className="text-violet-400 uppercase hover:text-violet-500 text-sm">{review.job}</p>
 				</div>
 
 				<div className="text-violet-500 mx-auto mt-5">
 					<FaQuoteLeft></FaQuoteLeft>
 				</div>
 
-				<div className=" text-center mt-4 text-slate-500">{review.text}</div>
+				<div className=" text-center mt-4 text-slate-500 hover:text-violet-700">{review.text}</div>
 
 				<div className="text-violet-500 mx-auto mt-5">
 					<FaQuoteRight></FaQuoteRight>
 				</div>
 
-				<div
-					className="flex
-                 text-3xl mt-5 gap-3 text-violet-400 font-bold justify-center">
-					<button className="cursor-pointer hover:text-violet-500">
-						<FiChevronLeft></FiChevronLeft>
-					</button>
-					<button>
-						<FiChevronRight className="cursor-pointer hover:text-violet-500"></FiChevronRight>
-					</button>
-				</div>
-
-				<div>
-					<button>Surprise Me</button>
-				</div>
 			</div>
 		);
 }
--- React/testimonial_slider/src/components/Testimonial.js
+++ React/testimonial_slider/src/components/Testimonial.js
@@ -1,13 +1,63 @@
-import React from 'react'
-import Card from './Card';
+/** @format */
+
+import React, { useState } from "react";
+import Card from "./Card";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 function Testimonial(props) {
-    let reviews = props.reviews;
-    return <div>
-        
-            <Card review={reviews[1]}></Card>
-        
-    </div>;
+	let reviews = props.reviews;
+
+	const [cardIdx, setCardIdx] = useState(0);
+
+	function leftShiftHanlder() {
+		if (cardIdx == 0) {
+			setCardIdx(reviews.length-1);
+		} else {
+			setCardIdx(cardIdx - 1);
+		}
+	}
+
+	function rightShiftHanlder() {
+		if (cardIdx == reviews.length-1) {
+			setCardIdx(0);
+		} else {
+			setCardIdx(cardIdx + 1);
+		}
+	}
+
+    function supriseHandler() {
+        let idx = Math.floor(Math.random() * reviews.length);
+        setCardIdx(idx);
+    }
+
+	return (
+		<div className="w-[85vw] sm:w-[600px] bg-white flex flex-col justify-center items-center mt-10 p-10 transition-all duration-700 shadow-lg hover:drop-shadow-2xl">
+			<Card review={reviews[cardIdx]}></Card>
+
+			<div className="mt-[10px]">
+				<div 
+					className="mt-[20px] flex
+                 text-3xl gap-3 text-violet-400 font-bold justify-center">
+					<button
+						onClick={leftShiftHanlder}
+						className="cursor-pointer hover:text-violet-500">
+						<FiChevronLeft></FiChevronLeft>
+					</button>
+					<button onClick={rightShiftHanlder}>
+						<FiChevronRight className="cursor-pointer hover:text-violet-500"></FiChevronRight>
+					</button>
+				</div>
+
+				<div className="mx-auto">
+					<button
+						onClick={supriseHandler}
+						className="bg-violet-400 hover:bg-violet-600 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg mx-auto mt-[25px]">
+						Surprise Me
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Testimonial
+export default Testimonial;
