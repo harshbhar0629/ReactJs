fcef92e0d9c041347a3e962f33b5a2fb56e56ee0
harshbhar0629 <harshbhar.0629@gmail.com>
2024-04-28 23:44:02 +0530

    Add code for input form :

--- React_Intermediate/form_app/src/App.js
+++ React_Intermediate/form_app/src/App.js
@@ -3,27 +3,46 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  // if we have less no of arguements then we can use UseState hook but if we have multiple element then we can't afford much variable we can make but it is complex to maintain so we use objects
 
-  useEffect(() => {
-    setFirstName("");
-    setLastName("");
-  }, []);
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
 
-  function changeHandler1(event) {
-    // console.log("First Name: ", event.target.value);
-    // event.target represent the element
-    console.log("First Name: ", event.target);
-	}
-  function changeHandler2(event) {
-		console.log("Last Name: ", event.target.value);
-	}
+  // useEffect(() => {
+  //   setFirstName("");
+  //   setLastName("");
+  // }, []);
+
+  // function changeHandler1(event) {
+  //   // console.log("First Name: ", event.target.value);
+  //   // event.target represent the element
+  //   setFirstName(event.target.value);
+  //   // console.log("First Name: ", event.target);
+	// }
+  // function changeHandler2(event) {
+  //   // console.log("Last Name: ", event.target.value);
+  //   setLastName(event.target.value);
+	// }
+
+  // use Objects for handling multiple states or element
+  const [formData, setFormData] = useState({ firstName: "", lastName: "" });
+
+  function changeHandler(event) {
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [event.target.name] : event.target.value
+      }
+    })
+  }
 
   return (
 		<div className="App">
 			<form>
-				<input
+				{/*
+          both input for useState
+        
+          <input
 					type="text"
           placeholder="Enter First name.."
           onChange={changeHandler1}
@@ -32,7 +51,26 @@ function App() {
 					type="text"
           placeholder="Enter Last name.."
           onChange={changeHandler2}
-          ></input>
+          ></input> */}
+
+				<input
+					type="text"
+          placeholder="Enter First name.."
+          onChange={changeHandler}
+          name="firstName"
+          value={formData.firstName}
+        >
+        </input>
+        
+        <input
+          onChange={changeHandler}      
+          type='text'
+          placeholder='Enter Last Name..'
+          name='lastName'
+          value={formData.lastName}
+        >
+        </input>
+            
 			</form>
 		</div>
 	);
