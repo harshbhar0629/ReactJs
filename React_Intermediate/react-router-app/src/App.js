978892368ce9a27b18c50e23123436540b0cc047
harshbhar0629 <harshbhar.0629@gmail.com>
2024-04-29 22:50:35 +0530

    Learn about Routes:

--- React_Intermediate/react-router-app/src/App.js
+++ React_Intermediate/react-router-app/src/App.js
@@ -1,12 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+/**
+ * Routes => it creates multiple routes
+ * Route => its a single route
+ * Route => means path for single page application
+ *
+ * <Route path="/" element={<div>Home Page</div>}></Route>
+ * what it does => if path having only slash then it return element or render element
+ *
+ * <Route path="/support" element={<div>Support Page</div>}></Route>
+ *
+ * what it does => when our path is support then dupport page render
+ *
+ * @returns
+ */
 
 function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route
+					path="/"
+					element={<div>Home Page</div>}></Route>
+				<Route
+					path="/support"
+					element={<div>Support Page</div>}></Route>
+				<Route
+					path="/about"
+					element={<div>About Page</div>}></Route>
+				<Route
+					path="/labs"
+					element={<div>Labs Page</div>}></Route>
+				<Route
+					path="*"
+					element={<div>Invalid Path</div>}></Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
