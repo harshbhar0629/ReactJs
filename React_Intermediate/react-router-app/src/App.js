9fa971e7b031c0d3ee7f783b8c07a697425766b6
harshbhar0629 <harshbhar.0629@gmail.com>
2024-04-29 23:54:43 +0530

    Learn about Links and NavLinks in react-route-dom:

--- React_Intermediate/react-router-app/src/App.js
+++ React_Intermediate/react-router-app/src/App.js
@@ -3,6 +3,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Labs from "./components/Labs";
+import NotFound from "./components/NotFound";
+import Support from "./components/Support";
+import { Link, NavLink } from "react-router-dom";
 
 /**
  * Routes => it creates multiple routes
@@ -15,6 +21,12 @@ import { Routes, Route } from "react-router-dom";
  * <Route path="/support" element={<div>Support Page</div>}></Route>
  *
  * what it does => when our path is support then dupport page render
+ * 
+ * <Link to="/support">Support</Link>
+ * what it does => it link the routes means whatever path is described in routes to render its element you have to just mention in link whwn we click on link element then it hit routes path and routes element render on screen
+ * 
+ * 
+ * link vs navlink the main difference is that link render routes element but navlink actually add the active class to its rendering element if another element render then its  remove active class and add active to another rendering routes
  *
  * @returns
  */
@@ -22,7 +34,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
 	return (
 		<div className="App">
-			<Routes>
+			{/* <Routes>
 				<Route
 					path="/"
 					element={<div>Home Page</div>}></Route>
@@ -38,6 +50,41 @@ function App() {
 				<Route
 					path="*"
 					element={<div>Invalid Path</div>}></Route>
+			</Routes> */}
+
+			<nav>
+				<ul>
+					<li>
+						<NavLink to="/">Home</NavLink>
+					</li>
+					<li>
+						<NavLink to="/support">Support</NavLink>
+					</li>
+					<li>
+						<NavLink to="/about">About</NavLink>
+					</li>
+					<li>
+						<NavLink to="/labs">Labs</NavLink>
+					</li>
+				</ul>
+			</nav>
+
+			<Routes>
+				<Route
+					path="/"
+					element={<Home></Home>}></Route>
+				<Route
+					path="/support"
+					element={<Support></Support>}></Route>
+				<Route
+					path="/about"
+					element={<About></About>}></Route>
+				<Route
+					path="/labs"
+					element={<Labs></Labs>}></Route>
+				<Route
+					path="*"
+					element={<NotFound></NotFound>}></Route>
 			</Routes>
 		</div>
 	);
---
+++ React_Intermediate/react-router-app/src/components/Labs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Labs() {
+  return (
+    <div>Labs</div>
+  )
+}
+
+export default Labs
