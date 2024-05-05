e031cd86fe72e1d0b785401d002a9034fe3de668
harshbhar0629 <harshbhar.0629@gmail.com>
2024-05-05 09:34:41 +0530

    Add whole logic to generate random Gifs:

--- React_Advance/random-gif/src/App.js
+++ React_Advance/random-gif/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import Random from "./components/Random";
+import Tag from "./components/Tag";
+
+export default function App() {
+	return (
+		<div className="w-full min-h-screen p-8 flex flex-col background relative overflow-x-hidden items-center">
+			<h1
+				className=" bg-white rounded-xl text-center w-full
+       px-10 py-3 text-xl lg:text-3xl font-bold ">
+				RANDOM GIFS
+			</h1>
+			<div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
+				<Random />
+				<Tag />
+			</div>
+		</div>
+	);
 }
 
-export default App;
+// export default App;
---
+++ React_Advance/random-gif/src/components/Spinner.js
@@ -0,0 +1,13 @@
+/** @format */
+
+// import React from 'react'
+
+const Spinner = () => {
+	return (
+		<div>
+			<div className="spinner"></div>
+		</div>
+	);
+};
+
+export default Spinner;
