c2b52db2ebebbd06ccf1bfbf5194f517a312919d
harshbhar0629 <harshbhar.0629@gmail.com>
2024-05-06 15:27:08 +0530

    add code for blogs-api context:

--- React_Advance/blog-context/src/App.js
+++ React_Advance/blog-context/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import "./App.css"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Blogs from "./components/Blogs"
 
 function App() {
   return (
     <div className="App">
-      App
+      <Header></Header>
+      <Blogs></Blogs>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ React_Advance/blog-context/src/context/AppContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+// step-1 context creation
+export const AppContext = createContext();
+
+// step-2 provider
+// function AppContext
