01febb6efa0778e7f3f5fa63d5a9c373abaecc39
harshbhar0629 <harshbhar.0629@gmail.com>
2024-05-22 02:06:32 +0530

    Update changes:

--- React_Advance/shopping-cart/src/redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { CartSlice } from "./slice/CartSlice"
-
-export const store = configureStore({
-    reducer: {
-        cart: CartSlice
-    }
-});
