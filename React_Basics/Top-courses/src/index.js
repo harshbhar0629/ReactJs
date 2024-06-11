414d0906a16d62c082412a49ac782079b346bcb0
harshbhar0629 <harshbhar.0629@gmail.com>
2024-06-11 12:57:58 +0530

    Add codes:

--- React_Basics/Top-courses/src/index.js
+++ React_Basics/Top-courses/src/index.js
@@ -4,11 +4,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ToastContainer } from "react-toastify";
-
+import { Toaster } from "react-hot-toast";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-		<ToastContainer>
+		<Toaster>
 			<App></App>
-		</ToastContainer>
+		</Toaster>
 );
